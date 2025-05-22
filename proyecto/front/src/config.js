b7c519b0b79d0470d83b5202bcbadb5c99478229
Rafael Ramos Llanos <dr0573131@gmail.com>
2025-05-21 21:27:00 -0300

    ad

--- proyecto/front/src/config.js
+++ proyecto/front/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:3001'
+  apiUrl: process.env.REACT_APP_API_URL || 'https://prueva2-backend.onrender.com'
 };
 
 export default config;
